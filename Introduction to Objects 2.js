281533c457030c5517451a545345f6cc3b46d488
Geoff Russell <Geoff.R.Russell@gmail.com>
2014-12-08 22:03:07 -0800

    Completed Introduction to Objects 2

--- Introduction to Objects 2.js
+++ Introduction to Objects 2.js
@@ -74,19 +74,80 @@
 // gary.meow();
 
 // 20/30
-function Penguin(name) {
-    this.name = name;
-    this.numLegs = 2;
+// function Penguin(name) {
+//     this.name = name;
+//     this.numLegs = 2;
+// }
+
+// // create your Emperor class here and make it inherit from Penguin
+
+// function Emperor(name){
+//     this.name = name;
+// }
+
+// Emperor.prototype = new Penguin();
+
+// // create an "emperor" object and print the number of legs it has
+// emperor = new Emperor("ssl");
+// console.log(emperor.numLegs);
+
+// 27/30
+// var languages = {
+//     english: "Hello!",
+//     french: "Bonjour!",
+//     notALanguage: 4,
+//     spanish: "Hola!"
+// };
+
+// // print hello in the 3 different languages
+// for(var item in languages){
+//     if (typeof languages[item] === "string"){
+//         console.log(languages[item]);
+//     }
+// }
+
+// 28/30
+// function Dog (breed) {
+//     this.breed = breed;
+// }
+
+// // add the sayHello method to the Dog class
+// // so all dogs now can say hello
+// Dog.prototype.sayHello = function(){
+// 	console.log("Hello this is a " + this.breed + " dog");
+// };
+
+// var yourDog = new Dog("golden retriever");
+// yourDog.sayHello();
+
+// var myDog = new Dog("dachshund");
+// myDog.sayHello();
+
+// 29/30
+// what is this "Object.prototype" anyway...?
+// var prototypeType = typeof Object.prototype;
+// console.log(prototypeType);
+
+// // now let's examine it!
+// var hasOwn = Object.prototype.hasOwnProperty('hasOwnProperty');
+// console.log(hasOwn);
+
+// 30/30
+function StudentReport() {
+    var grade1 = 4;
+    var grade2 = 2;
+    var grade3 = 1;
+    this.getGPA = function() {
+        return (grade1 + grade2 + grade3) / 3;
+    };
 }
 
-// create your Emperor class here and make it inherit from Penguin
+var myStudentReport = new StudentReport();
 
-function Emperor(name){
-    this.name = name;
+for(var x in myStudentReport) {
+    if(typeof myStudentReport[x] !== "function") {
+        console.log("Muahaha! " + myStudentReport[x]);
+    }
 }
 
-Emperor.prototype = new Penguin();
-
-// create an "emperor" object and print the number of legs it has
-emperor = new Emperor("ssl");
-console.log(emperor.numLegs);
+console.log("Your overall GPA is " + myStudentReport.getGPA());
